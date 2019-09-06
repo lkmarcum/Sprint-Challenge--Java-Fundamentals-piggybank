cf18550bc5bba9b96869b97f97bf22787c31e8f2
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-09-06 10:36:55 -0500

    populated array list w/ coins; successfully printed coins in bank

--- piggyBank/src/com/lambdaschool/Main.java
+++ piggyBank/src/com/lambdaschool/Main.java
@@ -8,8 +8,14 @@ public class Main {
     {
 	    ArrayList<Coins> piggyBank = new ArrayList<Coins>();
 
-	    piggyBank.add(new Penny(5));
+	    piggyBank.add(new Quarter());
+	    piggyBank.add(new Dime());
+	    piggyBank.add(new Dollar(5));
+	    piggyBank.add(new Nickel(3));
+	    piggyBank.add(new Dime(7));
+	    piggyBank.add(new Dollar());
+	    piggyBank.add(new Penny(10));
 
-	    piggyBank.forEach(c -> System.out.println("test: " + c.getNumAdded()));
+	    piggyBank.forEach(c -> System.out.println(c.getNumAdded()));
     }
 }
