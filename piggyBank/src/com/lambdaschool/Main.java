0f5969e9c3c200800a6d8ca916e45c51b7798078
lkmarcum <50113705+lkmarcum@users.noreply.github.com>
2019-09-06 10:43:10 -0500

    added variable/loop to get total in bank

--- piggyBank/src/com/lambdaschool/Main.java
+++ piggyBank/src/com/lambdaschool/Main.java
@@ -1,11 +1,13 @@
 package com.lambdaschool;
 
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args)
     {
+        DecimalFormat fp = new DecimalFormat("$###,###.00");
 	    ArrayList<Coins> piggyBank = new ArrayList<Coins>();
 
 	    piggyBank.add(new Quarter());
@@ -17,5 +19,13 @@ public class Main {
 	    piggyBank.add(new Penny(10));
 
 	    piggyBank.forEach(c -> System.out.println(c.getNumAdded()));
+
+	    double bankTotal = 0.00;
+	    for(int i = 0; i < piggyBank.size(); i++)
+        {
+            bankTotal += piggyBank.get(i).getTotal();
+        }
+
+        System.out.println("The piggy bank holds " + fp.format(bankTotal));
     }
 }
